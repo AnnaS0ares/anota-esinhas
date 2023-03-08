89b0505628a09c0b7684c47a77e98dc1af2a190a
annakd0 <annasoares.bb@gmail.com>
2023-03-08 11:54:49 -0300

    8 de março

--- python/aulapython/exerc.py
+++ python/aulapython/exerc.py
@@ -1,10 +1,19 @@
 peso= float(input("Qual é seu peso? "))
 altura= float(input("Qual é sua altura? "))
 
+
 imc= peso/(altura*altura)
-if imc < 30:   
+if imc > 18.5 and imc < 24.9:   
       print("  Você esta saudavel!  " )
-else:   
-      print( "Você não esta saudavel, reveja seus habitos!  " )
+elif imc  >  25 and imc< 29.9: 
+      print( "Você está com sobrepeso!  " )
+
+elif imc > 30 and imc< 34.4:
+     print ("voce é com obeidade 1! ") 
+if imc > 35:
+    print(" Você esta com obesidade 2! ")   
+
+
+
 print("seu imc é: ", imc)
 
